8dcf3ed798ce8cacb4c4d42a1ac74acb5e9fa620
root <root@localhost.localdomain>
2022-09-28 03:05:17 -0700

    deviceのインストールまで

--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,14 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_06_30_175452) do
-  create_table "dialies", charset: "utf8mb4", force: :cascade do |t|
+  create_table "dialies", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "hoges", charset: "utf8mb4", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", charset: "utf8mb4", force: :cascade do |t|
+  create_table "hoges", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
